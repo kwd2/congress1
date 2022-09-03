aa311353274343e5e2efe07036f268ded3ba0328
kwd2 <kendelsignore@gmail.com>
2022-09-03 18:16:11 -0500

    Add files via upload

---
+++ scr20full.sh
@@ -0,0 +1,17 @@
+cat \
+  data/by_date/itcont_18.txt \
+    | ./a.out >scr18_cmte_name.txt
+
+cat \
+  data/by_date/itcont_20.txt \
+    | ./a.out >scr20_cmte_name.txt
+
+cat \
+  data/by_date/itcont_22.txt \
+    | ./a.out >scr22_cmte_name.txt
+
+
+
+
+
+
